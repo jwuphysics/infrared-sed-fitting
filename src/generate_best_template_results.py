c468e1ba1a8b2aae711bdafba6895b53805f21aa
John <jwuphysics@gmail.com>
2017-09-06 16:51:36 -0400

    minor fix up + redo fitting based on change in measurements

--- src/generate_best_template_results.py
+++ src/generate_best_template_results.py
@@ -1,7 +1,7 @@
 """
 John F. Wu
 
-Given some infrared measurements and uncertainties, determine the 
+Given some infrared measurements and uncertainties, determine the
 best fit teplate SED and uncertainty in the total luminosity.
 """
 
@@ -13,15 +13,15 @@ import os
 root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 PLOT_RESULTS = True
-VERBOSE = True
+VERBOSE = False
 
-def assign_alma_wavelengths_by_redshift(z):                                                             
-    if z > 1.0:                                                                                       
-        return 1300         
-    elif z > 0.8:                                                                               
+def assign_alma_wavelengths_by_redshift(z):
+    if z > 1.0:
+        return 1300
+    elif z > 0.8:
         return 1178.4
-    else:                                                 
-        return nan  
+    else:
+        return np.nan
 
 if __name__ == '__main__':
 
@@ -29,22 +29,22 @@ if __name__ == '__main__':
     data_fname = os.path.join(root_dir, 'results', 'lascar_cluster_measurements.cat')
     lascar = np.genfromtxt(data_fname, usecols=(0, 3, 5, 6, 8, 9, 17, 18), dtype=None)
 
-    fig, axes = plt.subplots(4, 5, figsize=(20, 16), sharex=True, sharey=True)                              
+    fig, axes = plt.subplots(4, 5, figsize=(20, 16), sharex=True, sharey=True)
 
     for ax, g in zip(axes.flat, lascar):
         name, z, S_100, S_100_err, S_160, S_160_err, S_alma, S_alma_err = g
 
         if np.isnan(S_alma_err):
-            measurements = [[100, S_100, S_100_err], 
+            measurements = [[100, S_100, S_100_err],
                             [160, S_160, S_160_err]]
         else:
             wave_alma = assign_alma_wavelengths_by_redshift(z)
-            measurements = [[100, S_100, S_100_err], 
-                            [160, S_160, S_160_err], 
+            measurements = [[100, S_100, S_100_err],
+                            [160, S_160, S_160_err],
                             [wave_alma, S_alma, S_alma_err]]
-        
-        template, L_IR, norm = fit_sed.find_best_template(measurements, z, 
-                                NUM_BEST_TEMPLATES=3, visualize=PLOT_RESULTS, 
+
+        template, L_IR, norm = fit_sed.find_best_template(measurements, z,
+                                NUM_BEST_TEMPLATES=3, visualize=PLOT_RESULTS,
                                 ax=ax, verbose=VERBOSE)
 
         # get the model wavelengths and fluxes
